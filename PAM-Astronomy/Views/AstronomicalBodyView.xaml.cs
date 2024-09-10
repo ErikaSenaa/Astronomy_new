a83a0b360492e603e400df6fa1d3158d8f1e17bd
erika <erikasena.avila@gmail.com>
2024-09-10 12:39:26 -0300

    atualizacao

--- PAM-Astronomy/Views/AstronomicalBodyView.xaml.cs
+++ PAM-Astronomy/Views/AstronomicalBodyView.xaml.cs
@@ -9,33 +9,40 @@ public partial class AstronomicalBodyView : ContentPage
     string astroName;
     public string AstroName
     {
-        get => astroName;
-        set
+    get => astroName;
+    set
         {
             astroName = value;
             UpdateAstroBodyUI(astroName);
         }
     }
+    [QueryProperty(nameof(AstroName), "astroName")]
+    public partial class AstronomicalBodyPage
+    {
+
+    }
+
+    public object SolarSystemData { get; private set; }
+
     void UpdateAstroBodyUI(string astroName)
     {
         AstronomicalBody body = FindAstroData(astroName);
+
         Title = body.Name;
+
         lblIcon.Text = body.EmojiIcon;
         lblName.Text = body.Name;
         lblMass.Text = body.Mass;
         lblCircumference.Text = body.Circumference;
         lblAge.Text = body.Age;
     }
-    AstronomicalBody FindAstroData(string astronomicalBodyName)
+    AstronomicalBody FindAstroData(string astronomicalBodyName) => astronomicalBodyName switch
     {
-        return astronomicalBodyName switch
-        {
-            "comet" => SolarSystemData.HalleysComet,
-            "earth" => SolarSystemData.Earth,
-            "moon" => SolarSystemData.Moon,
-            "sun" => SolarSystemData.Sun,
-            _ => throw new ArgumentException()
-        };
-    }
+        "comet" => SolarSystemData.HalleysComet,
+        "earth" => SolarSystemData.Earth,
+        "moon" => SolarSystemData.Moon,
+        "sun" => SolarSystemData.Sun,
+        _ => throw new ArgumentException()
+    };
 
 }
